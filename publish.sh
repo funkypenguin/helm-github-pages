f0257f4b78861368e8a03663f5ab410658f9146f
David Young <davidy@funkypenguin.co.nz>
2019-05-01 12:36:42 +1200

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -9,14 +9,14 @@ WORKING_DIRECTORY="$PWD"
   exit 1
 }
 [ -z "$GITHUB_PAGES_BRANCH" ] && GITHUB_PAGES_BRANCH=gh-pages
-[ -z "$HELM_CHARTS_SOURCE" ] && HELM_CHARTS_SOURCE="$WORKING_DIRECTORY/charts"
+[ -z "$HELM_CHARTS_SOURCE" ] && HELM_CHARTS_SOURCE="$WORKING_DIRECTORY/helm-chart"
 [ -d "$HELM_CHARTS_SOURCE" ] || {
-  echo "ERROR: Could not find Helm charts in $HELM_CHARTS_SOURCE"
+  echo "ERROR: Could not find Helm chart in $HELM_CHARTS_SOURCE"
   exit 1
 }
 [ -z "$HELM_VERSION" ] && HELM_VERSION=2.8.1
-[ "$CIRCLE_BRANCH" ] || {
-  echo "ERROR: Environment variable CIRCLE_BRANCH is required"
+[ "$TRAVIS_BRANCH" ] || {
+  echo "ERROR: Environment variable TRAVIS_BRANCH is required"
   exit 1
 }
 
@@ -24,24 +24,10 @@ echo "GITHUB_PAGES_REPO=$GITHUB_PAGES_REPO"
 echo "GITHUB_PAGES_BRANCH=$GITHUB_PAGES_BRANCH"
 echo "HELM_CHARTS_SOURCE=$HELM_CHARTS_SOURCE"
 echo "HELM_VERSION=$HELM_VERSION"
-echo "CIRCLE_BRANCH=$CIRCLE_BRANCH"
-
-echo '>> Prepare...'
-mkdir -p /tmp/helm/bin
-mkdir -p /tmp/helm/publish
-apk update
-apk add ca-certificates git openssh
-
-echo '>> Installing Helm...'
-cd /tmp/helm/bin
-wget "https://storage.googleapis.com/kubernetes-helm/helm-v${HELM_VERSION}-linux-amd64.tar.gz"
-tar -zxf "helm-v${HELM_VERSION}-linux-amd64.tar.gz"
-chmod +x linux-amd64/helm
-alias helm=/tmp/helm/bin/linux-amd64/helm
-helm version -c
-helm init -c
+echo "TRAVIS_BRANCH=$TRAVIS_BRANCH"
 
 echo ">> Checking out $GITHUB_PAGES_BRANCH branch from $GITHUB_PAGES_REPO"
+mkdir /tmp/helm/publish
 cd /tmp/helm/publish
 mkdir -p "$HOME/.ssh"
 ssh-keyscan -H github.com >> "$HOME/.ssh/known_hosts"
@@ -49,8 +35,6 @@ git clone -b "$GITHUB_PAGES_BRANCH" "git@github.com:$GITHUB_PAGES_REPO.git" .
 
 echo '>> Building charts...'
 find "$HELM_CHARTS_SOURCE" -mindepth 1 -maxdepth 1 -type d | while read chart; do
-  echo ">>> helm lint $chart"
-  helm lint "$chart"
   chart_name="`basename "$chart"`"
   echo ">>> helm package -d $chart_name $chart"
   mkdir -p "$chart_name"
@@ -59,15 +43,15 @@ done
 echo '>>> helm repo index'
 helm repo index .
 
-if [ "$CIRCLE_BRANCH" != "master" ]; then
+if [ "$TRAVIS_BRANCH" != "master" ]; then
   echo "Current branch is not master and do not publish"
   exit 0
 fi
 
 echo ">> Publishing to $GITHUB_PAGES_BRANCH branch of $GITHUB_PAGES_REPO"
-git config user.email "$CIRCLE_USERNAME@users.noreply.github.com"
-git config user.name CircleCI
+git config user.email "$TRAVIS_BUILD_NUMBER@users.noreply.github.com"
+git config user.name "Travis CI"
 git add .
 git status
-git commit -m "Published by CircleCI $CIRCLE_BUILD_URL"
+git commit -m "Published by TravisCI $TRAVIS_BUILD_WEB_URL"
 git push origin "$GITHUB_PAGES_BRANCH"
