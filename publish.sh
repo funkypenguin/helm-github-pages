a5f978d15813e2e6f5426f884d9e250951e7306a
David Young <davidy@funkypenguin.co.nz>
2019-05-02 13:12:59 +1200

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -15,6 +15,7 @@ WORKING_DIRECTORY="$PWD"
   exit 1
 }
 [ -z "$HELM_VERSION" ] && HELM_VERSION=2.13.1
+[ -z "$KUBERNETES_VERSION" ] && KUBERNETES_VERSION=1.14.0
 [ "$CIRCLE_BRANCH" ] || {
   echo "ERROR: Environment variable CIRCLE_BRANCH is required"
   exit 1
@@ -24,19 +25,29 @@ echo "GITHUB_PAGES_REPO=$GITHUB_PAGES_REPO"
 echo "GITHUB_PAGES_BRANCH=$GITHUB_PAGES_BRANCH"
 echo "HELM_CHARTS_SOURCE=$HELM_CHARTS_SOURCE"
 echo "HELM_VERSION=$HELM_VERSION"
+echo "KUBERNETES_VERSION=$KUBERNETES_VERSION"
 echo "CIRCLE_BRANCH=$CIRCLE_BRANCH"
+echo "PATH=$PATH"
 
 echo '>> Prepare...'
+mkdir -p /tmp/helm/bin
+mkdir -p /tmp/helm/publish
+mkdir -p /tmp/kubeval/bin
+mkdir -p /tmp/kubeval/manifests
+
 apk update
 apk add ca-certificates git openssh bash curl
 
 echo '>> Installing Helm...'
+cd /tmp/helm/bin
 wget "https://storage.googleapis.com/kubernetes-helm/helm-v${HELM_VERSION}-linux-amd64.tar.gz"
 tar -zxf "helm-v${HELM_VERSION}-linux-amd64.tar.gz"
 chmod +x linux-amd64/helm
 mv linux-amd64/helm /usr/local/bin/
 helm version -c
 helm init -c
+
+helm plugin install https://github.com/lrills/helm-unittest
 
 echo ">> Checking out $GITHUB_PAGES_BRANCH branch from $GITHUB_PAGES_REPO"
 mkdir -p /tmp/helm/publish
@@ -49,7 +60,15 @@ echo '>> Building charts...'
 find "$HELM_CHARTS_SOURCE" -mindepth 1 -maxdepth 1 -type d | while read chart; do
   echo ">>> helm lint $chart"
   helm lint "$chart"
+
+  echo ">>> kubeval $chart"
+  mkdir -p "/tmp/kubeval/manifests/$chart_name"
+  helm template $chart --output-dir "/tmp/kubeval/manifests/$chart_name"
+  find "/tmp/kubeval/manifests/$chart_name" -name '*.yaml' | grep -v crd | xargs kubeval -v $KUBERNETES_VERSION  
  
+  echo ">>> unittest $chart"
+  helm unittest $chart 
+  
   chart_name="`basename "$chart"`"
   echo ">>> helm package -d $chart_name $chart"
   mkdir -p "$chart_name"
